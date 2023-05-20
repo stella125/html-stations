7a30e654f495bc753fdd016826a3eef66ca07b1c
miyusuzuki <sole.luna.stella125@gmail.com>
2023-05-21 02:25:30 +0900

    feat: :sparkles: HTML/CSS/JavaScript入門編の課題をクリアする
    HTML/CSS/JavaScript入門編のStation01-15の課題をクリアしました

--- src/scripts/station15-common.js
+++ src/scripts/station15-common.js
@@ -1,5 +1,9 @@
 async function main() {
+    //console.log("A");
     const results = await getData();
+    //console.log("G");
+    //なんで上でawaitしているのに以降の行が先に走るのかわからない
+    //https://qiita.com/cheez921/items/41b744e4e002b966391a#asyncawait%E3%82%92%E4%BD%BF%E3%81%A3%E3%81%A6%E3%82%82%E3%81%A3%E3%81%A8%E7%B0%A1%E6%BD%94%E3%81%
     const elem = document.getElementById("result");
     results.forEach(result => {
         let li = document.createElement('li');
--- src/scripts/station15.js
+++ src/scripts/station15.js
@@ -2,15 +2,29 @@ async function getData() {
   // Promiseを使った実装をやってみましょう！APIとの通信でよく使う表現になります！
   // ３秒後にフルネームのリストを表示されるようにしましょう。
   // 最初から表示されていると、クリアになりません。
+  //console.log("B");
   const userList = [
     { id: 1, first_name: '優', family_name: '大木', affiliation: 'TechTrain', is_student: false },
     { id: 2, first_name: '太郎', family_name: '山田', affiliation: 'HogeHoge大学', is_student: true }
   ];
-  const result = await test()
+  //console.log("C");
+  const result = await test(userList);
+  //console.log("F");
   return await result
 }
 
-function test() {
-  return
+async function test(testList) {
+  //console.log("D");
+  return new Promise(function(resolve){
+    setTimeout(() => {
+      testList.map(
+        (user) => {
+          user.full_name = user.family_name + " " + user.first_name;
+        }
+      )
+      console.log("E");
+      resolve(testList);
+    },3000)
+  })
 }
 
